e77cd3db03293d593652fc9f3ed3844ff1c324b8
fherranzs <franciscoherranz@MacBook-Pro-de-Francisco.local>
2017-04-06 14:53:20 +0200

    Update 6-04

--- ours/afoc_transmitter.m
+++ ours/afoc_transmitter.m
@@ -20,34 +20,21 @@ lam = 1550;         %Central wavelength [nm]
 Ppeak = 42.7266;    %Peak power (from ex_01)
 symbrate = 10;      %Baud rate [Gbaud]
 duty = 1;           %Duty Cicle
-roll = 0.1;         %pulse roll-off
+roll = 0.2;         %pulse roll-off
 
 laser_signal = afoc_lasersource(Ppeak, lam, spac, Nfft);
 
 %Then, create the bit pattern and the electrical signal
 
-%Seguir aquí. Lo último es que ahora tenemos cuatro secuencias
-%de bits con la 16QAM.
+%OOK
+seq1 = pattern('random');
+seq2 = pattern('random');
 
-%OOK!!!___
-%seq1 = pattern('random');
-%seq2 = pattern('random');
+%OOK
+elec_i = electricsource(seq1, 'qpsk', symbrate, 'cosroll', duty, roll);
+elec_q = electricsource(seq2, 'qpsk', symbrate, 'cosroll', duty, roll);
 
-%16QAM!!!___
-OPTIONS.alphabet=16;
-[PAT,BMAT] = pattern('random',OPTIONS);
-
-%OOK!!!___
-%elec_i = electricsource(seq1, 'qpsk', symbrate, 'cosroll', duty, roll);
-%elec_q = electricsource(seq2, 'qpsk', symbrate, 'cosroll', duty, roll);
-
-%16QAM!!!___
-elec_i = electricsource(BMAT(:,1), 'qpsk', symbrate, 'cosroll', duty, roll);
-elec_q = electricsource(BMAT(:,2), 'qpsk', symbrate, 'cosroll', duty, roll);
-
-%OOK!!!___
-%[Eoutiq, Eopti, Eoptq] = afoc_iqmod(laser_signal, elec_i, elec_q, 1, 0);
-%16QAM!!!___
+%OOK
 [Eoutiq, Eopti, Eoptq] = afoc_iqmod(laser_signal, elec_i, elec_q, 1, 0);
 points=linspace(1,4096,4096);
 figure;
@@ -55,7 +42,6 @@ plot(points, elec_i, points, elec_q);
 legend('Electrical Signal (I-Branch)', 'Electrical Signal (Q-Branch)');
 figure;
 plot(points, Eoutiq);
-%plot(Eopti,Eoptq,'.b');
 legend('IQ Modulator output')
 
 %One last step, in order to compare with Optilux results.
@@ -66,5 +52,4 @@ legend('Optilux','Afoc IQ')
 
 x_axis = linspace(-5,5, length(Eopti));
 figure;
-%plot(x_axis,Eopti, 'b.')
-plot(real(Eoutiq),imag(Eoutiq),'.');
+plot(real(Eoutiq),imag(Eoutiq),'*');
