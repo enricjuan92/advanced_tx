26776f7f4639bd61a414d3cc05a5ac622f633c66
Enric Juan <enricjuan@MacBook-Pro-de-Enric.local>
2017-04-06 14:59:10 +0200

    voy a dar por culo

--- ours/afoc_mzm.m
+++ ours/afoc_mzm.m
@@ -6,9 +6,9 @@ V_l = -elec/2;
 Ph_u = pi*(V_u-Vbias)/Vpi;
 Ph_l = pi*(V_l+Vbias)/Vpi;
 
-%Eout = -1i*Ein.*0.5.*(exp(1i*Ph_u) - exp(1i*Ph_l));
+Eout = -1i*Ein.*0.5.*(exp(1i*Ph_u) - exp(1i*Ph_l)); %Nominally
 %Eout = 2i./((exp(1i*Eout_p) - exp(1i*(-Eout_p))));
-Eout = Ein.*asin(0.5*(Ph_u-Ph_l)).*exp(1i*(0.5*(Ph_u+Ph_l)));
+%Eout = Ein.*asin(0.5*(Ph_u-Ph_l)).*exp(1i*(0.5*(Ph_u+Ph_l)));
 %Eout = asin(Eout_p);
 
 
