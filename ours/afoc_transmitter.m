25076c9fea0640e119396190097da6c077b80c8b
fherranzs <franciscoherranz@MacBook-Pro-de-Francisco.local>
2017-04-06 17:31:03 +0200

    Update 6-04 v2

--- ours/afoc_transmitter.m
+++ ours/afoc_transmitter.m
@@ -27,18 +27,31 @@ laser_signal = afoc_lasersource(Ppeak, lam, spac, Nfft);
 %Then, create the bit pattern and the electrical signal
 
 %OOK
-seq1 = pattern('random');
-seq2 = pattern('random');
+%seq1 = pattern('random');
+%seq2 = pattern('random');
+
+%16QAM
+OPTIONS.alphabet = 4;
+seq1 = pattern('debruijn', 0, OPTIONS);
+seq2 = pattern('debruijn', 1, OPTIONS);
 
 %OOK
-%elec_i = electricsource(seq1, 'qpsk', symbrate, 'cosroll', duty, roll);
-%elec_q = electricsource(seq2, 'qpsk', symbrate, 'cosroll', duty, roll);
-modsignal = modulationChooser(seq1, 'QPSK');
-elec_i = real(modsignal);
-elec_q = imag(modsignal);
+% elec_i = electricsource(seq1, 'qpsk', symbrate, 'cosroll', duty, roll);
+% elec_q = electricsource(seq2, 'qpsk', symbrate, 'cosroll', duty, roll);
+%modsignal = modulationChooser(seq1, 'QPSK');
+%elec_i = real(modsignal);
+%elec_q = imag(modsignal);
+
+%16QAM
+par.alphabet = 4;
+par.limits = [-1;1];
+elec_i = electricsource(seq1, 'userdef', symbrate, 'cosroll', duty, roll, par);
+elec_q = electricsource(seq2, 'userdef', symbrate, 'cosroll', duty, roll, par);
+
 
 %OOK
 [Eoutiq, Eopti, Eoptq] = afoc_iqmod(laser_signal, elec_i, elec_q, 1, 0);
+%points = linspace(1, length(modsignal), length(modsignal));
 points=linspace(1,4096,4096);
 figure;
 plot(points, elec_i, points, elec_q);
@@ -46,16 +59,28 @@ legend('Electrical Signal (I-Branch)', 'Electrical Signal (Q-Branch)');
 figure;
 plot(points, Eoutiq);
 legend('IQ Modulator output')
-elec_complex = elec_i + 1i*elec_q;
 figure;
-plot(real(elec_complex), imag(elec_complex));
+plot(elec_i, elec_q, '*r');
+title('Electrical constellation');
 
 %One last step, in order to compare with Optilux results.
 Eoptilux = qi_modulator(laser_signal, elec_i, elec_q);
 figure;
 plot(points, Eoptilux, points, Eoutiq);
 legend('Optilux','Afoc IQ')
-
-x_axis = linspace(-5,5, length(Eopti));
 figure;
-plot(real(Eoutiq),imag(Eoutiq),'*');
+plot(real(Eoptilux), imag(Eoptilux), '*g');
+
+% figure;
+% plot(real(Eoutiq),imag(Eoutiq),'*');
+% title('Optical constellation');
+
+figure;
+h=animatedline(real(Eoutiq(1)), imag(Eoutiq(1)), 'LineStyle', 'None', 'Marker', '*', 'Color', 'blue');
+axis([-8 8 -8 8]);
+for i=2:4096
+    addpoints(h, real(Eoutiq(i)), imag(Eoutiq(i)))
+    drawnow
+end
+
+% length(find(abs(Eoutiq)>6))/length(Eoutiq) *100
