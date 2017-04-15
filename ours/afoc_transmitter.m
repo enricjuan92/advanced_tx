992d36246165ace0cb2118705c9f3c56f27ef1b4
Enric Juan <enricjuan@MacBook-Pro-de-Enric.local>
2017-04-15 13:41:29 +0200

    added results folder with minor modifications for saving

--- ours/afoc_transmitter.m
+++ ours/afoc_transmitter.m
@@ -25,14 +25,19 @@ ELECS.symbrate = 10;      %Baud rate [Gbaud]
 ELECS.duty = 1;           %Duty Cicle
 ELECS.roll = 0.2;         %pulse roll-off
 
+%Modulator parameters
+Vpi = 1;
+Vbias = 0;
+%modulation = 'ook';
+%modulation = 'qpsk';
+modulation = '16qam';
+
 %% Laser source generation
 laser_signal = afoc_lasersource(Ppeak, lam, spac, Nfft);
 
 %% Create the bit pattern and the electrical signal
 
-%[elec_i, elec_q] = elec_signal('qpsk', ELECS);
-%[elec_i, elec_q] = elec_signal('16qam', ELECS);
-[elec_i, elec_q] = elec_signal('ook', ELECS);
+[elec_i, elec_q] = elec_signal(modulation, ELECS);
 
 % Plot Electrical signal
 points = linspace(1, length(elec_i), length(elec_i));
@@ -41,7 +46,7 @@ figure;
 plot(points, elec_i, points, elec_q);
 title('Electrical Signals');
 legend('Electrical Signal (I-Branch)', 'Electrical Signal (Q-Branch)');
-saveas(gcf, 'electrical_signals.png');
+saveas(gcf, strcat('Results/',modulation, '_', 'electrical_signals.png'));
 
 %Plot electrical complex constellation
 % Preprocessing
@@ -57,10 +62,10 @@ ax.YAxisLocation = 'origin';
 axis([-1.5 1.5 -1.5 1.5]);
 title('Electrical constellation');
 hold off;
-saveas(gcf,'electrical_constellation.png');
+saveas(gcf, strcat('Results/',modulation, '_', 'electrical_constellation.png'));
 
 %% IQ Modulation by means of a MZM 
-Eoutiq = afoc_iqmod(laser_signal, elec_i, elec_q, 1, 0);
+Eoutiq = afoc_iqmod(laser_signal, elec_i, elec_q, Vpi, Vbias);
 
 %Plot IQ Modulator output
 figure;
@@ -73,7 +78,22 @@ title('Q Modulator output');
 subplot(2,2,[3 4])
 plot(points, Eoutiq);
 title('IQ Modulator output');
-saveas(gcf, 'IQ_Modulator_output.png')
+saveas(gcf, strcat('Results/',modulation, '_', 'IQ_Modulator_output.png'));
+
+% Plot Optical Constellation (Optilux)
+% Preprocessing
+Eout_i = real(Eoutiq);
+Eout_q = imag(Eoutiq);
+
+[index_i_resize, index_q_resize] = isignal_reshape(Eout_i, Eout_q, 25);
+
+figure;
+plot(Eout_i, Eout_q, '*g');
+hold on;
+plot(Eout_i(index_i_resize), Eout_q(index_q_resize), '*r', 'LineWidth', 2);
+title('Optical constellation')
+hold off;
+saveas(gcf, strcat('Results/',modulation, '_', 'Optical_constellation.png'));
 
 %% Comparing results with Optilux toolbox
 Eoptilux = qi_modulator(laser_signal, elec_i, elec_q);
@@ -83,7 +103,7 @@ figure;
 plot(points, Eoptilux, points, Eoutiq);
 title('Transmissor Ouptut: AFOC vs. Optilux')
 legend('Optilux','Afoc IQ')
-saveas(gcf, 'Transmissor_comparison.png');
+saveas(gcf, strcat('Results/',modulation, '_', 'Transmissor_comparison.png'));
 % Podríamos normalizar las señales, restarlas y plotar la diferencia 
 
 % Plot Optical Constellation (Optilux)
@@ -99,7 +119,7 @@ hold on;
 plot(Eoptilux_i(index_i_resize), Eoptilux_q(index_q_resize), '*r', 'LineWidth', 2);
 title('Optical constellation (Optilux)')
 hold off;
-saveas(gcf, 'Optical_constellation.png');
+saveas(gcf, strcat('Results/',modulation, '_', 'Optical_constellation_optilux.png'));
 
 % Plot Optical Constellation animation (AFOC)
 animated_plot(Eoutiq, 'Optical constellation Animation (AFOC)', [-8 8 -8 8]);
