184ef86532874f23c78b7c19a8a43a430420be59
fherranzs <franciscoherranz@MacBook-Pro-de-Francisco.local>
2017-05-09 19:58:13 +0200

    Update 9/05. Integrationv2

---
+++ ours/electricalsource/detector.m
@@ -0,0 +1,14 @@
+function [detected_symbs] = detector(Ein)
+
+%Ein => Received signal after optoelectric conversion
+
+Ein_real = real(Ein);
+Ein_imag = imag(Ein);
+
+%Muestrear cada Nt a partir de Nt/2
+    %Un vector con la parte real de N símbolos
+    %Un vecotr con la parte imaginaria de N símbolos
+%Definimos los niveles y a partir de ahí las regiones
+
+end
+
